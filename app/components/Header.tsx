621872bd804fcde95471c9e921908ce5d86dbcbd
Md. Mamun <mkmamun031@gmail.com>
2024-07-13 20:31:32 +0600

    Added The profile section in Component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import { GridBackground } from './ui/GridBackground'
+import { FlipWords } from './ui/FlipWords'
+import { AnimatedTooltip } from './ui/Tooltip'
+import { FaDownload, FaFacebook, FaGithub, FaLinkedin, FaTwitter, FaVoicemail } from 'react-icons/fa6'
+import { TypewriterEffectSmooth } from './ui/TypeWriter'
+import { Profile } from './Profile'
 
 const Header = () => {
+    const words = ["Full Stack Web Developer", "MERN Stack Developer"];
+
     return (
-        <div>
-            <GridBackground />
+        <div className="text-center flex justify-between items-center w-full">
+            <div className="flex-1 w-full flex items-center gap-12 justify-center">
+                <div>
+                    <TypewriterEffectSmooth className="text-left" words={[
+                        { text: "Hi", className: "text-purple" },
+                        { text: ",", className: "text-purple" },
+                        { text: "I", className: "text-purple" },
+                        { text: "'", className: "text-purple" },
+                        { text: "m", className: "text-purple" },
+                        { text: " ", className: "text-purple" },
+                        { text: "Md.", className: "text-purple" },
+                        { text: "Mamun", className: "text-purple" },
+                    ]}
+                    />
+                    <div className="text-left mt-4">
+                        I&apos;m a
+                        <FlipWords words={words} /> <br />
+                        <p className="text-justify max-w-2xl  mt-4">Highly motivated Junior Software Developer skilled in HTML, CSS, JavaScript, DOM manipulation, OOP, ES6, React, and Tailwind CSS, with proficiency in Firebase, Next.js, Axios, Node.js, Vite, MongoDB, Express, and more. Seeking an entry-level position to enhance skills and contribute to impactful software projects.</p>
+                    </div>
+                    <div className="flex items-start justify-start gap-6 flex-col">
+                        <div className="flex items-start justify-start mt-4 gap-6">
+                            <a href="https://github.com/MamunKhan71"><AnimatedTooltip items={[{ id: 1, name: "Github", icon: <FaGithub className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
+                            <a href="https://www.linkedin.com/in/mdmamun99/"><AnimatedTooltip items={[{ id: 2, name: "Linkedin", icon: <FaLinkedin className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
+                            <a href="https://www.facebook.com/mkmamun111"><AnimatedTooltip items={[{ id: 3, name: "Facebook", icon: <FaFacebook className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
+                            <a href="https://x.com/MdMamun30226822"><AnimatedTooltip items={[{ id: 4, name: "Twitter", icon: <FaTwitter className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
+                            <a href="mailto:mkmamun031@gmail.com"><AnimatedTooltip items={[{ id: 4, name: "Email", icon: <FaVoicemail className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
+                        </div>
+                        <a href="MyResume.pdf" download={true}>
+                            <button className="inline-flex gap-2 items-center h-12 animate-shimmer justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                                Download My CV <FaDownload />
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="flex-1 w-full">
+                <Profile />
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,49 +8,10 @@ import { FaFacebook, FaGithub, FaLinkedin, FaTwitter, FaVoicemail } from "react-
 import { AnimatedTooltip } from "./components/ui/Tooltip";
 
 export default function Home() {
-  const words = ["Full Stack Web Developer", "MERN Stack Developer"];
   return (
     <div className="h-[50rem] max-w-[1550px] w-full mx-auto dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="text-center flex justify-between items-center w-full">
-        <div className="flex-1 w-full flex items-center gap-12 justify-center">
-          <div>
-            <TypewriterEffect className="text-left" words={[
-              { text: "Hi", className: "text-purple" },
-              { text: ",", className: "text-purple" },
-              { text: "I", className: "text-purple" },
-              { text: "'", className: "text-purple" },
-              { text: "m", className: "text-purple" },
-              { text: " ", className: "text-purple" },
-              { text: "Md.", className: "text-purple" },
-              { text: "Mamun", className: "text-purple" },
-            ]}
-            />
-            <div className="text-left mt-4">
-              I&apos;m a
-              <FlipWords words={words} /> <br />
-              <p className="text-justify max-w-2xl  mt-4">Highly motivated Junior Software Developer skilled in HTML, CSS, JavaScript, DOM manipulation, OOP, ES6, React, and Tailwind CSS, with proficiency in Firebase, Next.js, Axios, Node.js, Vite, MongoDB, Express, and more. Seeking an entry-level position to enhance skills and contribute to impactful software projects.</p>
-            </div>
-            <div className="flex items-start justify-start gap-6 flex-col">
-              <div className="flex items-start justify-start mt-4 gap-6">
-                <a href="https://github.com/MamunKhan71"><AnimatedTooltip items={[{ id: 1, name: "Github", icon: <FaGithub className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
-                <a href="https://www.linkedin.com/in/mdmamun99/"><AnimatedTooltip items={[{ id: 2, name: "Linkedin", icon: <FaLinkedin className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
-                <a href="https://www.facebook.com/mkmamun111"><AnimatedTooltip items={[{ id: 3, name: "Facebook", icon: <FaFacebook className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
-                <a href="https://x.com/MdMamun30226822"><AnimatedTooltip items={[{ id: 4, name: "Twitter", icon: <FaTwitter className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
-                <a href="mailto:mkmamun031@gmail.com"><AnimatedTooltip items={[{ id: 4, name: "Email",  icon: <FaVoicemail className="w-1/2 h-1/2" color="rgb(71 85 105)" /> }]} /></a>
-              </div>
-              <a href="MyResume.pdf" download={true}>
-                <button className="inline-flex gap-2 items-center h-12 animate-shimmer justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                  Download My CV <FaDownload />
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="flex-1 w-full">
-          <Profile />
-        </div>
-      </div>
+      <Header/>
     </div>
   );
 }
