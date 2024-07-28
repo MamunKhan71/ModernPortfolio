2bf3bfc79b4664826066dd27736c89fad24e0c83
Md. Mamun <mkmamun031@gmail.com>
2024-07-28 12:47:46 +0600

    Fixed Profile.tsx

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -8,7 +8,15 @@ export function Profile() {
             <div className=" w-full relative max-w-md">
                 <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-black to-blue-800 transform scale-[0.90] bg-amber-500 rounded-full  blur-3xl" />
                 <div className="relative">
-                    <GlareCard children={<Image src={'/profile-black.png'} height={1000} width={1000} alt="profile-image" className="rounded-[var(--radius)]  z-10 p-2" />} />
+                    <GlareCard>
+                        <Image
+                            src={'/profile-black.png'}
+                            height={1000}
+                            width={1000}
+                            alt="profile-image"
+                            className="rounded-[var(--radius)] z-10 p-2"
+                        />
+                    </GlareCard>
                     {/* <Button duration={5000} children={<Image src={'/profile-black.png'} height={1000} width={1000} alt="profile-image" className="rounded-[1.75rem] z-10 p-2" />} /> */}
                 </div>
             </div>
