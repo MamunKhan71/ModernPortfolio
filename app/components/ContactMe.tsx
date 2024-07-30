84b8e370ea9021773860ca6371bb3c8d4136f0c4
Md. Mamun <mkmamun031@gmail.com>
2024-07-30 08:16:33 +0600

    Added netlify Form

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -42,7 +42,7 @@ export function ContactMe() {
         <div className="space-y-12" id="contact">
             <TextGenerateEffect words="Let's get in touch" className="text-4xl text-center" />
             <div className="w-full max-w-5xl mx-auto rounded-none md:rounded-2xl p-4 md:p-12 shadow-input  border-[0.2px] border-slate-800">
-                <form className="my-8" onSubmit={handleSubmit}>
+                <form className="my-8" onSubmit={handleSubmit} name="contact" netlify>
                     <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
                         <LabelInputContainer>
                             <Label htmlFor="name">Name</Label>
