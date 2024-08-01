1c570e532822d0bffcd0c92ffd4190ae39e02056
Md. Mamun <mkmamun031@gmail.com>
2024-08-01 14:22:05 +0600

    Minor Bug Fixes

--- app/components/ui/BentoGrid.tsx
+++ app/components/ui/BentoGrid.tsx
@@ -43,7 +43,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 d dark:border-white/[0.2] border border-transparent justify-between flex flex-col space-y-3",
+        "row-span-1  bg-black rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 d dark:border-white/[0.2] border border-transparent justify-between flex flex-col space-y-3",
         className
       )}
     >
