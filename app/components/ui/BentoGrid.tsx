cc187da7dc5008b742e8338cc032235cd967dbc1
Md. Mamun <mkmamun031@gmail.com>
2024-08-02 12:31:35 +0600

    Minor Responsiveness Fixes

--- app/components/ui/BentoGrid.tsx
+++ app/components/ui/BentoGrid.tsx
@@ -12,7 +12,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-3 gap-8 ",
+        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ",
         className
       )}
     >
