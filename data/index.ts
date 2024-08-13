7cdaca9caf02e86b4d8bf26006209b7c4737b02c
Md. Mamun <mkmamun031@gmail.com>
2024-08-14 00:53:53 +0600

    Remodified the navigation bar

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Education", link: "#education" },
-    { name: "Technologies", link: "#tech" },
+    // { name: "Technologies", link: "#tech" },
     { name: "Projects", link: "#projects" },
     { name: "Process", link: "#process" },
     { name: "Contact", link: "#contact" },
